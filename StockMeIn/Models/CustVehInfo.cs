15b99bbf0aa3f38a42819cb18ecfde7ec944c917
kasey-halesPBC <kasey.hales@mymail.champlain.edu>
2021-07-25 16:05:38 -0600

    Added message details, validation, and started maintenance page.

--- StockMeIn/Models/CustVehInfo.cs
+++ StockMeIn/Models/CustVehInfo.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 namespace StockMeIn.Models
 {
     public class CustVehInfo
-    {
+    {  // Model to display customer vehicle info. Contains a vehicle object and a customer vehicle
+       // to join the two and display proper vehicle information
         public Vehicle VehicleInfo { get; set; }
         public CustomerVehicle CustVehData { get; set; }
     }
--- StockMeIn/Models/Customer.cs
+++ StockMeIn/Models/Customer.cs
@@ -20,33 +20,41 @@ namespace StockMeIn.Models
         public string MiddleName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "Must Be Capitalized 2 Letter State Code")]
+        // Validate that state is 2 characters display message
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Must Be Minimum 2 Letter State Code")]
+        // Validate that stat only contain capital letters
         [RegularExpression(@"[A-Z]*$", ErrorMessage = "Must Be Capitalized 2 Letter State Code")]
         public string State { get; set; }
-        [StringLength(5, MinimumLength = 5, ErrorMessage = "Must Be 5 Digit Numeric Zip Code")]
+        // Validate that zip code is a minimum of 5 character display message
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "Must Be minumum 5 Digits")]
+        // Validate that zip only contains numbers
         [RegularExpression(@"[0-9]*$", ErrorMessage = "Must Be 5 Digit Numeric Zip Code")]
         public string Zip { get; set; }
         [Display(Name = "Home Phone")]
         [DataType(DataType.PhoneNumber)]
+        // Validate phone is 3 numbers and a - three numbers and a - then 4 numbers
         [RegularExpression(@"^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$", 
             ErrorMessage = "Must be in Format ###-###-####")]
         public string HomePhone { get; set; }
         [Display(Name = "Work Phone")]
         [DataType(DataType.PhoneNumber)]
+        // Validate phone is 3 numbers and a - three numbers and a - then 4 numbers
         [RegularExpression(@"^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$",
             ErrorMessage = "Must be in Format ###-###-####")]
         public string WorkPhone { get; set; }
         [Display(Name = "Cell Phone")]
         [DataType(DataType.PhoneNumber)]
+        // Validate phone is 3 numbers and a - three numbers and a - then 4 numbers
         [RegularExpression(@"^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$",
             ErrorMessage = "Must be in Format ###-###-####")]
         public string CellPhone { get; set; }
         public string Notes { get; set; }
+        // Set a date datatype
         [DataType(DataType.Date)]
         [Display(Name = "Created")]
         [Required]
         public DateTime CreateDAte { get; set; }
-
+        // Validate email address. must contain @ and .com, .edu, etc.
         [RegularExpression
             (@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
             ErrorMessage = "Invalid Email Address")]
